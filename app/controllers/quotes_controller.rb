951b9da690e549277f51732cbf78440c3107a86c
carlos <csolorio05@gmail.com>
2015-08-23 18:01:05 -0400

    minor end

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -74,27 +74,28 @@ class QuotesController < ApplicationController
         password_field.value = ENV['thumbtack_pw']
       end.submit
 
-    lead_page = m.get('http://thmtk.com/Yy2XwJr2')
-    parsed_page = lead_page.parser            
-    
-    q = Quote.new        
-    q.type_of_home = parsed_page.css('.request-info')[1].text
-    q.recurrence = parsed_page.css('.request-info')[2].text
-    q.day_preference = parsed_page.css('.request-info')[3].text
-    q.time_preference = parsed_page.css('.request-info')[4].text
-    q.bedrooms = parsed_page.css('.request-info')[5].text
-    q.bathrooms = parsed_page.css('.request-info')[6].text
-    q.square_feet = parsed_page.css('.request-info')[7].text
-    q.cleaning_supplies = parsed_page.css('.request-info')[8].text
-    # q.eco_friendly = parsed_page.css('.request-info')[2].text
-    # q.pets = parsed_page.css('.request-info')[2].text
-    # q.laundry = parsed_page.css('.request-info')[2].text
-    # q.refrigerator = parsed_page.css('.request-info')[2].text
-    # q.oven = parsed_page.css('.request-info')[2].text
-    # q.windows = parsed_page.css('.request-info')[2].text
+      lead_page = m.get('http://thmtk.com/Yy2XwJr2')
+      parsed_page = lead_page.parser            
+      
+      q = Quote.new        
+      q.type_of_home = parsed_page.css('.request-info')[1].text
+      q.recurrence = parsed_page.css('.request-info')[2].text
+      q.day_preference = parsed_page.css('.request-info')[3].text
+      q.time_preference = parsed_page.css('.request-info')[4].text
+      q.bedrooms = parsed_page.css('.request-info')[5].text
+      q.bathrooms = parsed_page.css('.request-info')[6].text
+      q.square_feet = parsed_page.css('.request-info')[7].text
+      q.cleaning_supplies = parsed_page.css('.request-info')[8].text
+      # q.eco_friendly = parsed_page.css('.request-info')[2].text
+      # q.pets = parsed_page.css('.request-info')[2].text
+      # q.laundry = parsed_page.css('.request-info')[2].text
+      # q.refrigerator = parsed_page.css('.request-info')[2].text
+      # q.oven = parsed_page.css('.request-info')[2].text
+      # q.windows = parsed_page.css('.request-info')[2].text
 
 
-    q.save
+      q.save
+    end
   
   end
 
