a7b96dfcef74458df1b3143d7b93469717ba29a3
carlos <csolorio05@gmail.com>
2015-08-23 20:10:32 -0400

    some stuff

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -67,7 +67,7 @@ class QuotesController < ApplicationController
 
   end
 
-  def obtain_thumbtack_quote_info(q)
+  def obtain_thumbtack_quote_info(quote)
   
     m = Mechanize.new
 
@@ -79,30 +79,87 @@ class QuotesController < ApplicationController
         password_field.value = ENV['thumbtack_pw']
       end.submit
 
-      lead_page = m.get(q.name)
+      lead_page = m.get(quote.name)
       parsed_page = lead_page.parser            
       
             
-      q.type_of_home = parsed_page.css('.request-info')[1].text
-      q.recurrence = parsed_page.css('.request-info')[2].text
-      q.day_preference = parsed_page.css('.request-info')[3].text
-      q.time_preference = parsed_page.css('.request-info')[4].text
-      q.bedrooms = parsed_page.css('.request-info')[5].text
-      q.bathrooms = parsed_page.css('.request-info')[6].text
-      q.square_feet = parsed_page.css('.request-info')[7].text
-      q.cleaning_supplies = parsed_page.css('.request-info')[8].text
-      # q.eco_friendly = parsed_page.css('.request-info')[2].text
-      # q.pets = parsed_page.css('.request-info')[2].text
-      # q.laundry = parsed_page.css('.request-info')[2].text
-      # q.refrigerator = parsed_page.css('.request-info')[2].text
-      # q.oven = parsed_page.css('.request-info')[2].text
-      # q.windows = parsed_page.css('.request-info')[2].text
+      quote.type_of_home = parsed_page.css('.request-info')[1].text
+      quote.recurrence = parsed_page.css('.request-info')[2].text
+      quote.day_preference = parsed_page.css('.request-info')[3].text
+      quote.time_preference = parsed_page.css('.request-info')[4].text
+      quote.bedrooms = parsed_page.css('.request-info')[5].text
+      quote.bathrooms = parsed_page.css('.request-info')[6].text
+      quote.square_feet = parsed_page.css('.request-info')[7].text
+      quote.cleaning_supplies = parsed_page.css('.request-info')[8].text
+      # quote.eco_friendly = parsed_page.css('.request-info')[2].text
+      # quote.pets = parsed_page.css('.request-info')[2].text
+      # quote.qndry = parsed_page.css('.request-info')[2].text
+      # quote.refrigerator = parsed_page.css('.request-info')[2].text
+      # quote.oven = parsed_page.css('.request-info')[2].text
+      # quote.windows = parsed_page.css('.request-info')[2].text
 
 
-      q.save
+      quote.save
     end
   
   end
+
+  def submit_quote(quote)
+
+    quote = Quote.find_by_id(quote)
+
+    if quote.bedrooms == "Studio"
+      amount = 119
+    elsif quote.bedrooms == "1 bedroom"
+      amount = 129
+    elsif quote.bedrooms == "2 bedrooms"
+      amount = 139
+    elsif quote.bedrooms == "3 bedrooms"
+      amount = 159
+    elsif quote.bedrooms == "4 bedrooms"
+      amount = 189
+    elsif quote.bedrooms == "5 bedrooms"
+      amount = 219
+    elsif quote.bedrooms == "5+ bedrooms"
+      amount = 249  
+    else
+      break
+    end
+
+    if quote.recurrence == "Every week"
+      discount = 20
+    elsif quote.recurrence == "Every other week"
+      discount = 15
+    elsif quote.recurrence == "Once a month"
+      amount = 10
+    else
+      break
+    end
+
+    quoted_value = amount * discount / 100
+
+    m = Mechanize.new
+
+    m.get("https://www.thumbtack.com/login") do |login_page|
+      loggedin_page = login_page.form_with(:id => 'login') do |form|
+        username_field = form.field_with(:id => 'login_email')
+        username_field.value = ENV['thumbtack_un']
+        password_field = form.field_with(:id => 'login_password')
+        password_field.value = ENV['thumbtack_pw']
+      end.submit
+
+      lead_page = m.get(quote.name).form_with(id: "quote-on-request") do |form|
+        form.field_with(:name => 'size').options[0].click
+        quote_price_field = form.field_with(:id => 'estimate_fixed_price_per_unit')
+        quote_price_field.value = quoted_value
+        quote_message_field = form.field_with(id: 'message')
+        quote_message_field.value = "Hi ,\nWe have a flat rate for a #{quote.bedrooms} bedroom, home of $#{quoted_value}.\nAnything else such as laundry, refrigerator, oven, and window cleaning is an extra $25.\nBook a cleaning by using our website at www.maidfox.com or call us at (480) 360-6243 (MAID) today.\nWe're a top rated Phoenix area service and look forward to working with you!\nThanks!\nCarlos"
+      end.submit      
+
+    end
+
+  end
+
 
   # DELETE /quotes/1
   # DELETE /quotes/1.json
@@ -122,6 +179,6 @@ class QuotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      params.require(:quote).permit(:name, :quote_type, :type_of_home, :recurrence, :day_preference, :time_preference, :bedrooms, :bathrooms, :square_feet, :cleaning_supplies, :eco_friendly, :laundry, :pets, :refrigerator, :oven, :windows, :location, :request_time)
+      params.require(:quote).permit(:name, :quote_type, :type_of_home, :recurrence, :day_preference, :time_preference, :bedrooms, :bathrooms, :square_feet, :cleaning_supplies, :eco_friendly, :qndry, :pets, :refrigerator, :oven, :windows, :location, :request_time)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
+  match 'submit_quote/:id', :to => 'quote#submit_quote', as: 'quote_submission'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
